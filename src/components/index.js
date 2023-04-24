64e58ef8c22d2fb5164b62cec8411a93d53cbb58
Andreyhuey <andreyhuey777@gmail.com>
2023-04-24 11:52:04 +0100

    added components for homepage

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as About } from "./About.jsx";
+export { default as Banner } from "./Banner.jsx";
+export { default as Contact } from "./Contact.jsx";
+export { default as Header } from "./Header.jsx";
+export { default as Nav } from "./Nav.jsx";
+export { default as Services } from "./Services.jsx";
+export { default as Work } from "./Work.jsx";
