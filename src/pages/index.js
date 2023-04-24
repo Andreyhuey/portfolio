cb9bf657064ef95d760d28f164748d8f0c964a49
Andreyhuey <andreyhuey777@gmail.com>
2023-04-24 13:02:35 +0100

    Assests Added & Nav completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
-import { Header, Nav } from "@/components";
+import { Nav } from "../components";
 
 export default function Home() {
   return (
     <>
-      <section></section>
+      {/* google fonts */}
+      <link
+        href="https://fonts.googleapis.com/css2?family=Aldrich&family=Orbitron:wght@400;500;600;700;800;900&family=Rajdhani:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
+      <div className="bg-gray-900 bg-no-repeat bg-cover overflow-hidden">
+        <Nav />
+        <div className="h-[4000px]"></div>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      primary: "Orbitron",
+      secondary: "Rajdhani",
+      tertiary: "Aldrich",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#0a0a0a",
+        accent: "#B809C3",
       },
+      backgroundImage: {
+        site: "url('./src/assets/site-bg.jpg')",
+        about: "url('./src/assets/about.png')",
+        services: "url('./src/assets/services.png')",
+      },
+    },
+    container: {
+      padding: {
+        DEFAULT: "15px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
     },
   },
   plugins: [],
-}
+};
