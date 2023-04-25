7cf2fa1d0e017c24c4e1aa0fe06bdc4ea3b67afc
Andreyhuey <andreyhuey777@gmail.com>
2023-04-25 21:46:18 +0100

    added "to" for react-scroll links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,29 +7,44 @@ import { Link } from "react-scroll";
 
 export default function Nav() {
   return (
-    <>
+    <div>
       <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
         <div className="container mx-auto">
           {/* nav inner */}
           <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
-            <Link className="cursor-pointer w-[60px] h-[60px]  flex items-center justify-center">
+            <Link
+              to="home"
+              className="cursor-pointer w-[60px] h-[60px]  flex items-center justify-center"
+            >
               <BiHomeAlt />
             </Link>
-            <Link className="cursor-pointer w-[60px] h-[60px]  flex items-center justify-center">
+            <Link
+              to="about"
+              className="cursor-pointer w-[60px] h-[60px]  flex items-center justify-center"
+            >
               <BiUser />
             </Link>
-            <Link className="cursor-pointer w-[60px] h-[60px]  flex items-center justify-center">
+            <Link
+              to="services"
+              className="cursor-pointer w-[60px] h-[60px]  flex items-center justify-center"
+            >
               <BsClipboardData />
             </Link>
-            <Link className="cursor-pointer w-[60px] h-[60px]  flex items-center justify-center">
+            <Link
+              to="work"
+              className="cursor-pointer w-[60px] h-[60px]  flex items-center justify-center"
+            >
               <BsBriefcase />
             </Link>
-            <Link className="cursor-pointer w-[60px] h-[60px]  flex items-center justify-center">
+            <Link
+              to="contact"
+              className="cursor-pointer w-[60px] h-[60px]  flex items-center justify-center"
+            >
               <BsChatSquare />
             </Link>
           </div>
         </div>
       </nav>
-    </>
+    </div>
   );
 }
