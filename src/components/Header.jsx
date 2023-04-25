4470ccc7c8f6ef863360d05c3185cb451d7c0bff
Andreyhuey <andreyhuey777@gmail.com>
2023-04-25 21:49:06 +0100

    imported react, exported default function with ID

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="bg-pink-300" id="header">
+      Header
+    </div>
+  );
+}
---
+++ src/components/Services.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Services() {
+  return (
+    <div className="section" id="services">
+      Services
+    </div>
+  );
+}
