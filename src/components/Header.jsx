654b5c72a0cd7884d08d6ff64f70f12fecde9e8a
Andreyhuey <andreyhuey777@gmail.com>
2023-04-26 15:56:18 +0100

    Logo and Resume button added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import Image from "next/image";
+// images
+import Logo from "../assets/logo.svg";
 
 export default function Header() {
   return (
-    <div className="bg-pink-300" id="header">
-      Header
-    </div>
+    <header className="py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          {/* logo */}
+          <a href="#">
+            <Image src={Logo} alt="logo" />
+          </a>
+          {/* button */}
+          <button className="btn btn-sm">
+            <a href="https://canva.com" target="_blank">
+              Resume
+            </a>
+          </button>
+        </div>
+      </div>
+    </header>
   );
 }
