ffc6b71c75e7841bdd8472f9cab7f428886bd701
Andreyhuey <andreyhuey777@gmail.com>
2023-06-05 18:19:05 +0100

    constants added for easier data modification

---
+++ src/constants/constants.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    title: "Marvel",
+    description:
+      "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+    image: "/images/1.png",
+    tags: ["Bootstrap", "React"],
+    source: "https://github.com",
+    visit: "https://google.com",
+    id: 0,
+  },
+  {
+    title: "E-Commerce",
+    description:
+      "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+    image: "/images/1.png",
+    tags: ["React", "Nextjs", "Sanity"],
+    source: "https://github.com",
+    visit: "https://google.com",
+    id: 1,
+  },
+  {
+    title: "Dashboard",
+    description:
+      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+    image: "/images/1.png",
+    tags: ["React", "TailwindCss", "Redux"],
+    source: "https://github.com",
+    visit: "https://google.com",
+    id: 2,
+  },
+  {
+    title: "Crypto",
+    description:
+      "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+    image: "/images/1.png",
+    tags: ["React", "Redux", "RapidAPI"],
+    source: "https://github.com",
+    visit: "https://google.com",
+    id: 3,
+  },
+  {
+    title: "User Auth",
+    description:
+      "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+    image: "/images/1.png",
+    tags: ["React", "Bootstrap", "Firebase"],
+    source: "https://github.com",
+    visit: "https://google.com",
+    id: 4,
+  },
+  {
+    title: "Health Insurance",
+    description:
+      "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+    image: "/images/1.png",
+    tags: ["React", "Bootstrap", "Firebase"],
+    source: "https://github.com",
+    visit: "https://google.com",
+    id: 5,
+  },
+];
