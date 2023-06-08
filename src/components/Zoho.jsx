dc2c5db2ab02d38090f6e5d1389b159c1ec553f6
Andreyhuey <andreyhuey777@gmail.com>
2023-06-08 07:41:11 +0100

    Zoho modified with clearly defined document object

--- src/components/Zoho.jsx
+++ src/components/Zoho.jsx
@@ -1,19 +1,20 @@
 const Zoho = () => {
-  var $zoho = $zoho || {};
-  $zoho.salesiq = $zoho.salesiq || {
-    widgetcode:
-      "674abb03b75b55f1453edb339290361dac44d3cc60e406b13aaf23ed85abe4e654782c14d5367aa05ac148e35e8087d0",
-    values: {},
-    ready: function () {},
-  };
-  var d = document;
-  s = d.createElement("script");
-  s.type = "text/javascript";
-  s.id = "zsiqscript";
-  s.defer = true;
-  s.src = "https://salesiq.zoho.com/widget";
-  t = d.getElementsByTagName("script")[0];
-  t.parentNode.insertBefore(s, t);
+  if (typeof window !== "undefined") {
+    const $zoho = $zoho || {};
+    $zoho.salesiq = $zoho.salesiq || {
+      widgetcode:
+        "674abb03b75b55f1453edb339290361dac44d3cc60e406b13aaf23ed85abe4e654782c14d5367aa05ac148e35e8087d0",
+      values: {},
+      ready: function () {},
+    };
+    const d = document.createElement("script");
+    s.type = "text/javascript";
+    s.id = "zsiqscript";
+    s.defer = true;
+    s.src = "https://salesiq.zoho.com/widget";
+    t = d.getElementsByTagName("script")[0];
+    t.parentNode.insertBefore(s, t);
+  }
 };
 
 export default Zoho;
