d9c871228ffd4225ad27a97f442cf48245be0c58
Andreyhuey <andreyhuey777@gmail.com>
2023-06-08 07:50:14 +0100

    client side application error fixed

--- src/components/Zoho.jsx
+++ src/components/Zoho.jsx
@@ -1,18 +1,18 @@
 const Zoho = () => {
   if (typeof window !== "undefined") {
-    const $zoho = $zoho || {};
+    let $zoho = window.$zoho || {};
     $zoho.salesiq = $zoho.salesiq || {
       widgetcode:
         "674abb03b75b55f1453edb339290361dac44d3cc60e406b13aaf23ed85abe4e654782c14d5367aa05ac148e35e8087d0",
       values: {},
       ready: function () {},
     };
-    const d = document.createElement("script");
+    const s = document.createElement("script");
     s.type = "text/javascript";
     s.id = "zsiqscript";
     s.defer = true;
     s.src = "https://salesiq.zoho.com/widget";
-    t = d.getElementsByTagName("script")[0];
+    const t = document.getElementsByTagName("script")[0];
     t.parentNode.insertBefore(s, t);
   }
 };
