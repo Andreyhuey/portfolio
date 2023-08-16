2cf049bf0dad20aced920cbaabfa966ab5b69ce4
Andreyhuey <andreyhuey777@gmail.com>
2023-08-16 12:15:02 +0100

    removed {whileInView} from variants

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { fadeIn } from "@/variants";
 
 export default function Contact() {
   return (
@@ -7,13 +6,7 @@ export default function Contact() {
       <div className="sm:container mx-auto">
         <div className="flex flex-col lg:flex-row gap-x-10">
           {/* text */}
-          <div
-            variants={fadeIn("right", 0.3)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, amount: 0.3 }}
-            className="flex-1 flex justify-start items-center"
-          >
+          <div className="flex-1 flex justify-start items-center">
             <div>
               <h4 className="text-xl uppercase text-accent font-medium mb-2 tracking-wide">
                 Get in touch
