d1d07a6bca7b46a142d152a286c28047c08dda03
andreyhuey <andreyhuey777@gmail.com>
2025-02-03 15:25:21 +0100

    feat: move Google site verification meta tag into Head component in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,11 +28,12 @@ export default function Document() {
   return (
     <Html lang="en">
       {/* google fonts */}
-      <meta
-        name="google-site-verification"
-        content="6fT63fDi5k7OS74LD6JbglvkwDKGTRyMFXM1XU22LBU"
-      />
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="6fT63fDi5k7OS74LD6JbglvkwDKGTRyMFXM1XU22LBU"
+        />
+      </Head>
       <link
         href="https://fonts.googleapis.com/css2?family=Aldrich&family=Orbitron:wght@400;500;600;700;800;900&family=Rajdhani:wght@300;400;500;600;700&display=swap"
         rel="stylesheet"
