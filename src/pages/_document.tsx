0fb1268dffa5b257373d3dd676cc7fd95381a40f
andreyhuey <andreyhuey777@gmail.com>
2025-03-06 08:37:44 +0100

    feat: add description and icon to metadata in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Ola-obaado Oluwadara's Portfolio",
+  icons: "public/assets/avatar.jpeg",
+  description: "Software Engineer",
   keywords: [
     "oluwadara",
     "ola-obaado",
