a8dc26c414b953ea96eb7decbd8bf57d106209cf
andreyhuey <andreyhuey777@gmail.com>
2025-03-19 15:27:36 +0100

    feat: add custom title to portfolio app and update ESLint rules for better variable handling

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Ola-obaado Oluwadara&apos;s Portfolio</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
